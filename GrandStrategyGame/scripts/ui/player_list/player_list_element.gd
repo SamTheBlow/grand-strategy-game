e9f9c0ede53b263e64ecdba0b052b6b6123fdde0
Samuel Deblois <pokeyoshi@hotmail.com>
2024-02-26 13:51:57 -0500

    Differentiate humans from AI on the player list

--- GrandStrategyGame/scripts/ui/player_list/player_list_element.gd
+++ GrandStrategyGame/scripts/ui/player_list/player_list_element.gd
@@ -3,29 +3,64 @@ extends Control
 ## Class for a player as displayed in the player list interface.
 
 
+@export_category("Child nodes")
+@export var color_rect: ColorRect
 @export var container: Control
 @export var arrow_label: Label
 @export var username_label: Label
 
+@export_category("Variables")
+@export var username_color_human: Color
+@export var username_color_ai: Color
+@export var bg_color_human: Color
+@export var bg_color_ai: Color
+
+
 var _player: Player
 
 
-func _on_username_changed(new_username: String) -> void:
-	username_label.text = new_username
+func _on_username_changed(_new_username: String) -> void:
+	_update_shown_username()
 
 
-func _on_player_turn_changed(player: Player) -> void:
-	if player == _player:
-		arrow_label.text = "->"
-	else:
-		arrow_label.text = ""
+func _on_player_turn_changed(playing_player: Player) -> void:
+	_update_turn_indicator(playing_player)
 
 
 ## To be called when this node is created.
 func init(player: Player, turn: GameTurn) -> void:
 	_player = player
+	
 	player.username_changed.connect(_on_username_changed)
-	_on_username_changed(player.username())
+	_update_shown_username()
+	
 	turn.player_changed.connect(_on_player_turn_changed)
-	_on_player_turn_changed(turn.playing_player())
+	_update_turn_indicator(turn.playing_player())
+	
+	_update_appearance()
+	
 	custom_minimum_size.y = container.size.y
+
+
+func _update_shown_username() -> void:
+	username_label.text = _player.username()
+
+
+func _update_turn_indicator(playing_player: Player) -> void:
+	if playing_player == _player:
+		arrow_label.text = "->"
+	else:
+		arrow_label.text = ""
+
+
+func _update_appearance() -> void:
+	if _player.is_human:
+		username_label.add_theme_color_override(
+				"font_color", username_color_human
+		)
+		color_rect.color = bg_color_human
+	else:
+		username_label.add_theme_color_override(
+				"font_color", username_color_ai
+		)
+		color_rect.color = bg_color_ai
