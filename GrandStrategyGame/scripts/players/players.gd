f848119e966240490691e59da893ced8a052b013
Samuel Deblois <pokeyoshi@hotmail.com>
2024-03-22 09:41:21 -0400

    Add button that adds new local players

--- GrandStrategyGame/scripts/players/players.gd
+++ GrandStrategyGame/scripts/players/players.gd
@@ -1,4 +1,5 @@
 class_name Players
+## Class responsible for some group of players.
 
 
 var players: Array[Player]
@@ -19,3 +20,20 @@ func number_of_humans() -> int:
 			output += 1
 	
 	return output
+
+
+## Provides the default username for a potential new player.
+## For example, if there are already players named "Player 1" and "Player 2",
+## then this function will return "Player 3".
+func new_default_username() -> String:
+	var player_number: int = 1
+	while true:
+		var is_number_already_used: bool = false
+		for player in players:
+			if player.default_username == "Player " + str(player_number):
+				is_number_already_used = true
+				player_number += 1
+				break
+		if not is_number_already_used:
+			break
+	return "Player " + str(player_number)
--- GrandStrategyGame/scripts/ui/player_list/player_list.gd
+++ GrandStrategyGame/scripts/ui/player_list/player_list.gd
@@ -10,9 +10,12 @@ extends Control
 @export var player_list_element: PackedScene
 @export var margin: Control
 @export var container: Control
+@export var add_player_button: Control
 
 @export_category("Variables")
 @export var margin_pixels: int = 16 : set = _set_margin_pixels
+
+var _players: Players
 
 
 func _ready() -> void:
@@ -24,15 +27,30 @@ func _on_viewport_size_changed() -> void:
 	_update_size()
 
 
+func _on_add_player_button_pressed() -> void:
+	var player := Player.new()
+	player.is_human = true
+	player.default_username = _players.new_default_username()
+	
+	_players.players.append(player)
+	## TODO don't hard code the maximum number of players
+	if _players.players.size() >= 11:
+		add_player_button.hide()
+	
+	_add_element(player)
+
+
 ## To be called when this node is created.
 func init(players: Array[Player], game_turn: GameTurn = null) -> void:
-	var element: PlayerListElement
+	_players = Players.new()
+	_players.players = players.duplicate()
+	
+	if game_turn:
+		add_player_button.hide()
+	
 	for i in players.size():
-		element = player_list_element.instantiate() as PlayerListElement
-		element.init(players[i])
-		if game_turn:
-			element.init_turn(game_turn)
-		container.add_child(element)
+		_add_element(players[i], game_turn)
+	
 	_update_size()
 
 
@@ -43,6 +61,15 @@ func _set_margin_pixels(value: int) -> void:
 	margin.offset_top = margin_pixels
 	margin.offset_bottom = -margin_pixels
 	_update_size()
+
+
+func _add_element(player: Player, game_turn: GameTurn = null) -> void:
+	var element := player_list_element.instantiate() as PlayerListElement
+	element.init(player)
+	if game_turn:
+		element.init_turn(game_turn)
+	container.add_child(element)
+	container.move_child(element, -2)
 
 
 ## Manually set this node's size
