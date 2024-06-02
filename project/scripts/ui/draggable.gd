0daf9e6b0cc57eb3b89de17d75f48c279b127f25
SamDb <pokeyoshi@hotmail.com>
2024-06-02 10:53:25 -0400

    Clean up Draggable class

--- project/scripts/ui/draggable.gd
+++ project/scripts/ui/draggable.gd
@@ -7,38 +7,33 @@ extends Control
 ## and you need to connect that signal to [method Draggable._on_dragged].
 ## You only need to check for when the mouse button is first pressed:
 ## this node will take care of checking if the button is held or released.
-# TODO this class needs rework: remove the unused signal; privatize stuff
 
 
-## This signal will trigger on every frame when this object is being dragged.
-signal is_dragged(this_object: Draggable)
-
-var is_being_dragged: bool = false
-var relative_starting_position := Vector2.ZERO
-var relative_mouse_origin := Vector2.ZERO
+var _is_being_dragged: bool = false
+var _relative_starting_position := Vector2.ZERO
+var _relative_mouse_origin := Vector2.ZERO
 
 
 func _process(_delta: float) -> void:
-	if is_being_dragged:
-		var relative_position: Vector2 = get_relative_mouse_position()
-		var delta_x: float = relative_position.x - relative_mouse_origin.x
-		var delta_y: float = relative_position.y - relative_mouse_origin.y
+	if _is_being_dragged:
+		var relative_position: Vector2 = _relative_mouse_position()
+		var delta_x: float = relative_position.x - _relative_mouse_origin.x
+		var delta_y: float = relative_position.y - _relative_mouse_origin.y
 		var relative_width: float = anchor_right - anchor_left
 		var relative_height: float = anchor_bottom - anchor_top
-		anchor_left = relative_starting_position.x + delta_x
+		anchor_left = _relative_starting_position.x + delta_x
 		anchor_left = clampf(anchor_left, 0, 1 - relative_width)
 		anchor_right = anchor_left + relative_width
-		anchor_top = relative_starting_position.y + delta_y
+		anchor_top = _relative_starting_position.y + delta_y
 		anchor_top = clampf(anchor_top, 0, 1 - relative_height)
 		anchor_bottom = anchor_top + relative_height
-		is_dragged.emit(self)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var event_typed := event as InputEventMouseMotion
 		if (
-				is_being_dragged
+				_is_being_dragged
 				and event_typed.button_mask == MOUSE_BUTTON_MASK_LEFT
 		):
 			get_viewport().set_input_as_handled()
@@ -48,10 +43,12 @@ func _unhandled_input(event: InputEvent) -> void:
 				not event_typed.pressed
 				and event_typed.button_index == MOUSE_BUTTON_LEFT
 		):
-			is_being_dragged = false
+			_is_being_dragged = false
 
 
-func get_relative_mouse_position() -> Vector2:
+## Returns the mouse position in the window as values from 0 to 1,
+## where 0 is the top/left and 1 is the bottom/right.
+func _relative_mouse_position() -> Vector2:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	var window_size: Vector2 = get_viewport().get_visible_rect().size
 	return Vector2(
@@ -62,7 +59,7 @@ func get_relative_mouse_position() -> Vector2:
 
 ## Only works for objects anchored to the top-left corner.
 ## Currently unused and WIP.
-func clamp_to(control: Control) -> void:
+func _clamp_to(control: Control) -> void:
 	var delta_x: float = anchor_left - control.anchor_right
 	var delta_y: float = anchor_top - control.anchor_bottom
 	if delta_x < 0 and delta_y < 0:
@@ -78,6 +75,6 @@ func clamp_to(control: Control) -> void:
 
 ## Connect signals to this
 func _on_dragged() -> void:
-	is_being_dragged = true
-	relative_starting_position = Vector2(anchor_left, anchor_top)
-	relative_mouse_origin = get_relative_mouse_position()
+	_is_being_dragged = true
+	_relative_starting_position = Vector2(anchor_left, anchor_top)
+	_relative_mouse_origin = _relative_mouse_position()
