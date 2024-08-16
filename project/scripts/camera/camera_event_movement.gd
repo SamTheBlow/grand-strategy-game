798dc8ffad5f0af0ebc7657bcec2d1b37aa761c9
SamDb <pokeyoshi@hotmail.com>
2024-08-16 16:57:19 -0400

    By default, no longer move the camera when a player's turn begins

--- project/scripts/camera/camera_event_movement.gd
+++ project/scripts/camera/camera_event_movement.gd
@@ -1,25 +1,35 @@
 class_name CameraEventMovement
 extends Node
 ## Responsible for moving the camera under certain events.
-##
-## Initially, moves the camera to the center of the world map.
-## When it's a new player's turn to play (see [GameTurn]), this node moves
-## the camera to one of the [Country]'s controlled provinces.
-## If their [Country] doesn't have control over any [Province],
-## or if the player is a spectator, it defaults to not moving.
+## These events can be enabled or disabled individually.
 
 
 @export var _game: Game
 @export var _camera: CustomCamera2D
 
+## If true, after the game loads,
+## the camera will start in the center of the world map.
+@export var _start_in_center_of_map: bool = false
+
+## If true, when it's a new player's turn to play (see [GameTurn]),
+## moves the camera to one of the [Country]'s controlled provinces.
+## If their [Country] doesn't have control over any [Province],
+## or if the player is a spectator, it defaults to not moving.
+@export var _move_to_playing_country: bool = false
+
 
 func _ready() -> void:
-	_camera.move_to(_camera.world_limits._limits.get_center())
+	if _start_in_center_of_map:
+		_camera.move_to(_camera.world_limits._limits.get_center())
+	
 	_game.turn.player_changed.connect(_on_turn_player_changed)
 	_on_turn_player_changed(_game.turn.playing_player())
 
 
 func _on_turn_player_changed(player: GamePlayer) -> void:
+	if not _move_to_playing_country:
+		return
+	
 	var country: Country = player.playing_country
 	if not country:
 		return
