ba1bc3adde76d77bd1839ebbf39bda545afc3701
SamDb <pokeyoshi@hotmail.com>
2024-11-25 16:20:15 -0500

    Update headless_game_test to work with the randomly generated map

--- project/source/debug/headless_game_test.gd
+++ project/source/debug/headless_game_test.gd
@@ -22,15 +22,23 @@ var _game: Game
 func _ready() -> void:
 	if is_disabled:
 		return
-	
+
 	var game_rules := GameRules.new()
 	game_rules.turn_limit_enabled.value = true
 	game_rules.turn_limit.value = 500
-	
-	var populated_game := GameLoadPopulated.new()
-	populated_game.load_game(load_file_path, game_rules)
-	_game = populated_game.result
-	
+
+	# Temporary
+	if load_file_path == "res://assets/save_files/test3.json":
+		var generated_game := GameLoadGenerated.new()
+		generated_game.load_game(
+				MapMetadata.from_file_path(load_file_path), game_rules
+		)
+		_game = generated_game.result
+	else:
+		var populated_game := GameLoadPopulated.new()
+		populated_game.load_game(load_file_path, game_rules)
+		_game = populated_game.result
+
 	print("[HeadlessGameTest] Running the game...")
 	_game.turn.turn_changed.connect(_on_turn_changed)
 	_game.game_over.connect(_on_game_over)
@@ -43,7 +51,7 @@ func _on_turn_changed(turn: int) -> void:
 
 func _on_game_over(winner_country: Country) -> void:
 	_game.turn.stop()
-	
+
 	var debug_text: String = "[HeadlessGameTest] "
 	debug_text += (
 			"The game is over! The winner is "
@@ -51,7 +59,7 @@ func _on_game_over(winner_country: Country) -> void:
 			+ "The game lasted " + str(_game.turn.current_turn() - 1)
 			+ " turn(s)."
 	)
-	
+
 	var pcpc := ProvinceCountPerCountry.new()
 	pcpc.calculate(_game.world.provinces.list())
 	for i in pcpc.countries.size():
@@ -59,13 +67,13 @@ func _on_game_over(winner_country: Country) -> void:
 				"\n- " + pcpc.countries[i].country_name + " controls "
 				+ str(pcpc.number_of_provinces[i]) + " province(s)."
 		)
-	
+
 	print(debug_text)
-	
+
 	if save_the_game:
 		var game_save := GameSave.new()
 		game_save.save_game(_game, save_file_path)
-		
+
 		if game_save.error:
 			print(
 					"[HeadlessGameTest] Failed to save the game: ",
