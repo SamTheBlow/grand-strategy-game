e4a16adfa2fc41d3a98ce1815d2ea0bb43c31701
SamDb <pokeyoshi@hotmail.com>
2024-11-29 15:32:44 -0500

    Fix some battles not occuring

--- project/source/combat/battle_detection.gd
+++ project/source/combat/battle_detection.gd
@@ -22,8 +22,11 @@ func _init(
 ## Checks for [Battles] that need to occur in given [Army]'s [Province].
 ## Makes the battles happen, when applicable.
 func _resolve_battles(army: Army) -> void:
+	# Armies may get removed from the list as they destroy each other,
+	# so it's important to duplicate the array.
 	var armies_in_province: Array[Army] = (
-			_armies_in_each_province.dictionary[army.province()].list
+			_armies_in_each_province
+			.dictionary[army.province()].list.duplicate()
 	)
 	for other_army in armies_in_province:
 		if Country.is_fighting(army.owner_country, other_army.owner_country):
