009e6c19599c038085b4af47735dc6ae799c15ad
SamTheBlow <pokeyoshi@hotmail.com>
2025-10-19 16:15:07 -0400

    Automatically remove armies with a non-existant province

--- project/source/armies/armies_in_each_province.gd
+++ project/source/armies/armies_in_each_province.gd
@@ -3,6 +3,8 @@ class_name ArmiesInEachProvince
 ##
 ## See also: [ArmiesInProvince]
 
+var _armies: Armies
+
 ## All provinces in the game are guaranteed to be in this dictionary.
 ## Also, -1 is a valid key. It gives the list of
 ## all armies that are not in any province.
@@ -10,6 +12,8 @@ var _map: Dictionary[int, ArmiesInProvince] = { -1: ArmiesInProvince.new() }
 
 
 func _init(provinces: Provinces, armies: Armies) -> void:
+	_armies = armies
+
 	for province in provinces.list():
 		_on_province_added(province)
 
@@ -47,7 +51,19 @@ func _on_province_added(province: Province) -> void:
 
 
 func _on_province_removed(province: Province) -> void:
+	if not _map.has(province.id):
+		push_error("Province is not in the list.")
+		return
+
+	# Immediately remove the province from the list.
+	var armies_in_province: ArmiesInProvince = _map[province.id]
 	_map.erase(province.id)
+
+	# Remove all armies that were on that province, one by one.
+	for i in armies_in_province.list.size():
+		var army_to_remove: Army = armies_in_province.list[-1]
+		armies_in_province.remove(army_to_remove)
+		_armies.remove_army(army_to_remove)
 
 
 func _on_army_added(army: Army) -> void:
@@ -58,7 +74,6 @@ func _on_army_added(army: Army) -> void:
 func _on_army_removed(army: Army) -> void:
 	army.province_changed.disconnect(_on_army_province_changed)
 	if not _map.has(army.province_id()):
-		push_warning("Province is not on the list.")
 		return
 	_map[army.province_id()].remove(army)
 
@@ -66,8 +81,9 @@ func _on_army_removed(army: Army) -> void:
 ## Adds the army to the new province's list.
 ## As for removing the army from the previous province's list,
 ## the previous province's list does this by itself.
+## If the army's province doesn't exist, removes the army.
 func _on_army_province_changed(army: Army) -> void:
 	if not _map.has(army.province_id()):
-		push_warning("Province is not on the list.")
+		_armies.remove_army(army)
 		return
 	_map[army.province_id()].add(army)
