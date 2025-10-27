1809947e2e910ee4baa519b1598f1d3399945a77
SamTheBlow <pokeyoshi@hotmail.com>
2025-10-27 11:03:05 -0400

    Fix province preview not scaling & clipping inner contents

--- project/source/provinces/visuals/province_visuals_2d.gd
+++ project/source/provinces/visuals/province_visuals_2d.gd
@@ -31,9 +31,6 @@ var province: Province:
 var is_preview: bool = false
 var preview_rect: Rect2
 
-var _preview_polygon: PackedVector2Array = []
-var _preview_position := Vector2.ZERO
-
 @onready var _outlined_polygon := %Polygon as OutlinedPolygon2D
 @onready var _collision_shape := %CollisionShape as CollisionPolygon2D
 @onready var _buildings := %Buildings as BuildingVisuals2D
@@ -95,7 +92,7 @@ func highlight_debug(
 	var debug_highlight := OutlinedPolygon2D.new()
 	debug_highlight.name = "DebugHighlight"
 	debug_highlight.color = Color(0.0, 0.0, 0.0, 0.0)
-	debug_highlight.polygon = _polygon()
+	debug_highlight.polygon = province.polygon().array
 	debug_highlight.outline_settings = outline_settings
 	add_child(debug_highlight)
 
@@ -109,13 +106,11 @@ func _update_province() -> void:
 		name = "NullProvince"
 		return
 
-	if is_preview:
-		_calculate_preview()
-
 	name = str(province.id)
-	position = _position()
-	_army_stack.position = province.position_army_host
+	position = Vector2.ZERO
+	scale = Vector2.ONE
 
+	_army_stack.position = province.position_army_host
 	_buildings.setup(province)
 
 	_update_polygon()
@@ -127,11 +122,10 @@ func _update_province() -> void:
 
 func _update_polygon() -> void:
 	if is_preview:
-		_calculate_preview()
-		position = _preview_position
+		_update_preview()
 
-	_outlined_polygon.polygon = _polygon()
-	_collision_shape.polygon = _polygon()
+	_outlined_polygon.polygon = province.polygon().array
+	_collision_shape.polygon = province.polygon().array
 
 
 func _update_shape_color() -> void:
@@ -141,9 +135,7 @@ func _update_shape_color() -> void:
 	)
 
 
-func _calculate_preview() -> void:
-	var polygon: PackedVector2Array = province.polygon().array
-
+func _update_preview() -> void:
 	# Get the boundaries
 	var no_data: bool = true
 	var leftmost_point: float
@@ -151,72 +143,52 @@ func _calculate_preview() -> void:
 	var topmost_point: float
 	var bottommost_point: float
 
-	for vertex in polygon:
-		var point_x: float = vertex.x
-		var point_y: float = vertex.y
-
+	for vertex in province.polygon().array:
 		# Initialization
 		if no_data:
-			leftmost_point = point_x
-			rightmost_point = point_x
-			topmost_point = point_y
-			bottommost_point = point_y
+			leftmost_point = vertex.x
+			rightmost_point = vertex.x
+			topmost_point = vertex.y
+			bottommost_point = vertex.y
 			no_data = false
 			continue
 
-		if point_x < leftmost_point:
-			leftmost_point = point_x
-		elif point_x > rightmost_point:
-			rightmost_point = point_x
-		if point_y < topmost_point:
-			topmost_point = point_y
-		elif point_y > bottommost_point:
-			bottommost_point = point_y
+		if vertex.x < leftmost_point:
+			leftmost_point = vertex.x
+		elif vertex.x > rightmost_point:
+			rightmost_point = vertex.x
+		if vertex.y < topmost_point:
+			topmost_point = vertex.y
+		elif vertex.y > bottommost_point:
+			bottommost_point = vertex.y
 
 	if no_data:
 		return
 
-	var width: float = rightmost_point - leftmost_point
-	var height: float = bottommost_point - topmost_point
-	var desired_width: float = preview_rect.size.x
-	var desired_height: float = preview_rect.size.y
+	var province_rect := Rect2(
+			leftmost_point,
+			topmost_point,
+			rightmost_point - leftmost_point,
+			bottommost_point - topmost_point
+	)
 
 	# Prevent division by zero
-	if (
-			width == 0.0
-			or height == 0.0
-			or desired_width == 0.0
-			or desired_height == 0.0
-	):
+	if province_rect.size.x == 0.0 or province_rect.size.y == 0.0:
 		return
 
-	# Multiply every vertex by the scale ratio
-	# (e.g. if the polygon is 3x bigger than it should be,
-	# then multiply every vertex by 1/3)
+	# Determine the scale ratio (e.g. if the province
+	# is 3x bigger than it should be, then we need to scale by 1/3)
 	# To preserve the aspect ratio,
-	# we need to multilply by the smallest of the two ratios.
-	# Then offset every vertex to move the polygon to the desired position.
-	var scale_ratio: float = (
-			minf(desired_width / width, desired_height / height)
+	# we need to multiply by the smallest of the two ratios.
+	var scale_ratio: float = minf(
+			preview_rect.size.x / province_rect.size.x,
+			preview_rect.size.y / province_rect.size.y
 	)
-	var offset := Vector2(
-			preview_rect.position.x - leftmost_point,
-			preview_rect.position.y - topmost_point
+	position = (
+			preview_rect.position + 0.5 * preview_rect.size
+			- scale_ratio * (province_rect.position + 0.5 * province_rect.size)
 	)
-	_preview_polygon = []
-	for i in polygon.size():
-		_preview_polygon.append((polygon[i] + offset) * scale_ratio)
-	_preview_position = (
-			0.5 * (preview_rect.size - Vector2(width, height) * scale_ratio)
-	)
-
-
-func _polygon() -> PackedVector2Array:
-	return _preview_polygon if is_preview else province.polygon().array
-
-
-func _position() -> Vector2:
-	return _preview_position if is_preview else Vector2.ZERO
+	scale = Vector2(scale_ratio, scale_ratio)
 
 
 func _connect_signals() -> void:
