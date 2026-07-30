7225e76f8701b18680ad56baad5bb6c2ad696935
SamTheBlow <pokeyoshi@hotmail.com>
2026-07-30 12:15:51 -0400

    Refactor editor province interface classes

--- project/source/editor/interfaces/provinces/province_list_element.gd
+++ project/source/editor/interfaces/provinces/province_list_element.gd
@@ -4,42 +4,20 @@ extends Control
 
 signal pressed(this: ProvinceListElement)
 
-var province: Province:
-	set(value):
-		if province != null:
-			province.name_changed.disconnect(_refresh_name)
-
-		province = value
-
-		_refresh()
-		province.name_changed.connect(_refresh_name)
+var province: Province
 
 @onready var _preview := %ProvincePreview as ProvincePreviewNode
 @onready var _name_label := %NameLabel as Label
 
 
 func _ready() -> void:
-	# This is just so that this node still works by itself in the Godot editor
-	if province == null:
-		province = Province.new()
-
-	_refresh()
-
-
-func _refresh() -> void:
-	_refresh_preview()
-	_refresh_name()
-
-
-func _refresh_preview() -> void:
-	if not is_node_ready():
-		return
 	_preview.setup(province)
+
+	_refresh_name()
+	province.name_changed.connect(_refresh_name)
 
 
 func _refresh_name(_new_name: String = "") -> void:
-	if not is_node_ready():
-		return
 	_name_label.text = province.name_or_default()
 
 
--- project/source/editor/interfaces/provinces/province_preview.gd
+++ project/source/editor/interfaces/provinces/province_preview.gd
@@ -3,20 +3,13 @@ extends Control
 
 const _PROVINCE_VISUALS_SCENE := preload("uid://cppfb8jwghnqt") as PackedScene
 
-var _province_visuals: ProvinceVisuals2D
-
-
-func _ready() -> void:
-	if _province_visuals != null:
-		add_child(_province_visuals)
-
 
 func setup(province: Province) -> void:
-	_province_visuals = (
-			_PROVINCE_VISUALS_SCENE.instantiate() as ProvinceVisuals2D
-	)
-	_province_visuals.province = province
-	_province_visuals.is_preview = true
-	_province_visuals.preview_rect = Rect2(Vector2.ZERO, size)
+	var visuals := _PROVINCE_VISUALS_SCENE.instantiate() as ProvinceVisuals2D
+	visuals.province = province
+	visuals.is_preview = true
+	visuals.preview_rect = Rect2(Vector2.ZERO, size)
 	if is_node_ready():
-		add_child(_province_visuals)
+		add_child(visuals)
+	else:
+		ready.connect(add_child.bind(visuals), ConnectFlags.CONNECT_ONE_SHOT)
