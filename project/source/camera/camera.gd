31f175b3c43293406f5e9eff7fd27897d1ef5eea
SamTheBlow <pokeyoshi@hotmail.com>
2026-08-06 12:53:17 -0400

    Refactor CameraZoom

--- project/source/camera/camera.gd
+++ project/source/camera/camera.gd
@@ -1,53 +1,45 @@
 class_name CustomCamera2D
 extends Camera2D
-## Keeps the camera's position inside given [WorldLimits].
+## Clamps the camera's position and zoom according to given [WorldLimits].
 ##
 ## WARNING: If you set this node's "position" property directly,
 ## it will not automatically stay in bounds. Because of this,
 ## please use [method CustomCamera2D.move_to] to move the camera.
 
+signal zoom_limits_changed(minimum_zoom: float, maximum_zoom: float)
+
 ## How far away from the world the camera can go,
 ## measured in window size (e.g. 0.5 is half a window size).
 @export var world_margin := Vector2(0.5, 0.5)
 
-## May be null.
-var world_limits: WorldLimits:
+## May be null, in which case there are no bounds.
+var world_limits: WorldLimits = null:
 	set(value):
 		if world_limits != null:
-			world_limits.current_limits_changed.disconnect(
-					_on_world_limits_changed
-			)
-			propagate_call(&"_disconnect_world_limits", [world_limits])
+			world_limits.current_limits_changed.disconnect(_refresh)
+
 		world_limits = value
+		_refresh()
+
 		if world_limits != null:
-			_reposition_in_bounds()
-			propagate_call(&"_connect_world_limits", [world_limits])
-			world_limits.current_limits_changed.connect(
-					_on_world_limits_changed
-			)
+			world_limits.current_limits_changed.connect(_refresh.unbind(1))
 
 
 func _ready() -> void:
+	_refresh()
+	get_viewport().size_changed.connect(_refresh)
+
+
+func _refresh() -> void:
 	_reposition_in_bounds()
 
-
-func move_to(new_position: Vector2) -> void:
-	position = new_position
-	_reposition_in_bounds()
-
-
-func move_to_world_center() -> void:
-	move_to(world_limits.center())
+	var minimum_zoom: float = _minimum_zoom()
+	var maximum_zoom: float = _maximum_zoom()
+	set_zoom_clamped(zoom.x, minimum_zoom, maximum_zoom)
+	zoom_limits_changed.emit(minimum_zoom, maximum_zoom)
 
 
-## Translates this node by the given offset in local coordinates,
-## with the camera's zoom taken into account.
-func translate_zoomed(position_offset: Vector2) -> void:
-	position += position_offset / zoom
-	_reposition_in_bounds()
-
-
-## Returns the given position contained within the camera limits.
+## Returns the given position contained within the world limits.
 ## The camera must be in the scene tree for this function to work.
 func position_in_bounds(input_position: Vector2) -> Vector2:
 	if not is_inside_tree():
@@ -56,8 +48,8 @@ func position_in_bounds(input_position: Vector2) -> Vector2:
 				+ "but the camera is not in the scene tree."
 		)
 		return input_position
+
 	if world_limits == null:
-		push_error("World limits is null.")
 		return input_position
 
 	# NOTE: all of this assumes the camera's anchor mode is Drag Center
@@ -85,14 +77,63 @@ func position_in_bounds(input_position: Vector2) -> Vector2:
 	return output
 
 
-## Puts the camera back in bounds.
-## Has no effect if the camera is not in the scene tree.
-func _reposition_in_bounds() -> void:
-	if not is_inside_tree() or world_limits == null:
-		return
+func move_to(new_position: Vector2) -> void:
+	position = new_position
+	_reposition_in_bounds()
 
+
+func move_to_world_center() -> void:
+	move_to(world_limits.center())
+
+
+## Translates this node by the given offset in local coordinates.
+func translate_not_zoomed(position_offset: Vector2) -> void:
+	position += position_offset
+	_reposition_in_bounds()
+
+
+## Translates this node by the given offset in local coordinates,
+## with the camera's zoom taken into account.
+func translate_zoomed(position_offset: Vector2) -> void:
+	position += position_offset / zoom
+	_reposition_in_bounds()
+
+
+func set_zoom_clamped(
+		zoom_value: float,
+		minimum_zoom: float = _minimum_zoom(),
+		maximum_zoom: float = _maximum_zoom()
+) -> void:
+	zoom = Vector2.ONE * clampf(zoom_value, minimum_zoom, maximum_zoom)
+
+
+## Ensures the camera stays in bounds.
+## No effect if the camera is not in the scene tree.
+func _reposition_in_bounds() -> void:
+	if not is_inside_tree():
+		return
 	position = position_in_bounds(position)
 
 
-func _on_world_limits_changed(_world_limits: WorldLimits) -> void:
-	_reposition_in_bounds()
+## Currently, you can zoom out such that the world takes half the screen.
+func _minimum_zoom() -> float:
+	if not is_inside_tree() or world_limits == null:
+		return -INF
+
+	# Prevent division by zero
+	if (
+			world_limits.width() == 0.0
+			or world_limits.height() == 0.0
+	):
+		return -INF
+
+	return 0.5 * minf(
+			get_viewport_rect().size.x / world_limits.width(),
+			get_viewport_rect().size.y / world_limits.height()
+	)
+
+
+## Currently, you can zoom in no further than some arbitrary value.
+func _maximum_zoom() -> float:
+	const MAXIMUM_ZOOM: float = 1.0
+	return maxf(MAXIMUM_ZOOM, _minimum_zoom())
--- project/source/camera/camera_zoom.gd
+++ project/source/camera/camera_zoom.gd
@@ -1,26 +1,45 @@
 class_name CameraZoom
 extends Node
-## Allows the player to zoom the camera in/out.
-## Automatically zooms in the direction of the cursor.
+## Zooms a camera in/out with the mouse wheel towards the cursor's position.
 ##
-## See this video for more information about some of the code:
+## Some of the code was originally borrowed from this video by Bramwell:
 ## https://www.youtube.com/watch?v=gpvLqLggJuk
 
-@export var _camera: CustomCamera2D
+## Emits the zoom to apply whenever its value changes.
+signal zoom_changed(zoom: float)
 
-## Try a value in-between 0 and 1.[br] If the value is too low or too high,
-## the camera will use the minimum or maximum zoom.
+## Emits at a fast rate with how far to pan the camera, in world units.
+signal pan_processed(offset: Vector2)
+
+## Try a value in-between 0 and 1.
 @export var default_zoom: float = 1.0
 
-## The camera will do its best to reach this amount of zoom.
-var _target_zoom: float = 1.0
-## The previous zoom target is used to correctly zoom at the cursor's location.
-var _previous_target: float = 1.0
-## The direction and magnitude of the camera's movement.
-var _camera_movement: Vector2 = Vector2.ZERO
+var _minimum_zoom: float = -INF
+var _maximum_zoom: float = INF
 
-## The limit on how close the camera can zoom in.
-var _maximum_zoom_limit: float = 1.0
+## The camera will do its best to reach this amount of zoom.
+var _target_zoom: float = default_zoom:
+	set(value):
+		_previous_target = _target_zoom
+		if is_equal_approx(_target_zoom, value):
+			return
+		_target_zoom = value
+		set_physics_process(not is_equal_approx(_current_zoom, _target_zoom))
+
+## Used to correctly zoom at the cursor's location.
+var _previous_target: float = default_zoom
+
+var _current_zoom: float = default_zoom:
+	set(new_value):
+		var old_value: float = _current_zoom
+		_current_zoom = new_value
+		set_physics_process(not is_equal_approx(_current_zoom, _target_zoom))
+		if not is_equal_approx(old_value, new_value):
+			zoom_changed.emit(_current_zoom)
+
+## How far the camera still needs to pan, in world units.
+var _movement: Vector2 = Vector2.ZERO
+
 ## How close/far the camera will zoom in/out each time.
 var _zoom_increment: float = 0.075
 ## How fast the camera zooms in/out.
@@ -31,134 +50,73 @@ var _zoom_away_from_center: bool = true
 
 
 func _ready() -> void:
-	# We have to wait one frame,
-	# otherwise the minimum zoom is calculated incorrectly.
-	await get_tree().process_frame
-
-	# Zoom the camera to the default value
-	_target_zoom = clampf(default_zoom, _minimum_zoom(), _maximum_zoom())
-	_previous_target = _target_zoom
-	_camera.zoom = Vector2.ONE * _target_zoom
-
-	get_tree().get_root().size_changed.connect(_on_screen_size_changed)
-	_connect_world_limits(_camera.world_limits)
+	# Apply default zoom provided in scene export variable
+	reset()
 
 
 func _physics_process(delta: float) -> void:
 	var weight: float = _zoom_rate * delta
 
-	# Apply the zoom
-	_camera.zoom = _camera.zoom.lerp(_target_zoom * Vector2.ONE, weight)
+	# Apply zoom lerp
+	_current_zoom = lerpf(_current_zoom, _target_zoom, weight)
 
-	# Move the camera in the direction of the cursor
-	var camera_position: Vector2 = _camera.position_in_bounds(_camera.position)
-	var camera_to: Vector2 = camera_position + _camera_movement
-	var lerped_position: Vector2 = _camera.position.lerp(camera_to, weight)
-	_camera.position = _camera.position_in_bounds(lerped_position)
-	_camera_movement = camera_to - _camera.position
-
-	set_physics_process(not is_equal_approx(_camera.zoom.x, _target_zoom))
+	# Pan the camera in the direction of the cursor
+	if _movement != Vector2.ZERO:
+		var step: Vector2 = _movement * weight
+		_movement -= step
+		pan_processed.emit(step)
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var event_typed := event as InputEventMouseButton
-		if event_typed.is_pressed():
-			if event_typed.button_index == MOUSE_BUTTON_WHEEL_UP:
-				_zoom_in(event_typed.position)
-			if event_typed.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				_zoom_out(event_typed.position)
+	if event is not InputEventMouseButton:
+		return
+	var event_typed := event as InputEventMouseButton
+
+	if event_typed.is_pressed():
+		if event_typed.button_index == MOUSE_BUTTON_WHEEL_UP:
+			_zoom_in(event_typed.position)
+		if event_typed.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			_zoom_out(event_typed.position)
+
+
+## Resets all internal data.
+func reset() -> void:
+	_minimum_zoom = -INF
+	_maximum_zoom = INF
+	_target_zoom = default_zoom
+	_previous_target = default_zoom
+	_current_zoom = default_zoom
+	_movement = Vector2.ZERO
+
+
+## Sets the allowed zoom range.
+func set_zoom_limits(minimum_zoom: float, maximum_zoom: float) -> void:
+	_minimum_zoom = minimum_zoom
+	_maximum_zoom = maximum_zoom
+	_current_zoom = clampf(_current_zoom, _minimum_zoom, _maximum_zoom)
+	_target_zoom = clampf(_target_zoom, _minimum_zoom, _maximum_zoom)
 
 
 func _zoom_in(mouse_position: Vector2) -> void:
-	_previous_target = _target_zoom
-	_target_zoom = minf(_target_zoom + _zoom_increment, _maximum_zoom())
-	_zoom_to_cursor(mouse_position)
-	set_physics_process(true)
+	_target_zoom = minf(_target_zoom + _zoom_increment, _maximum_zoom)
+	_add_movement(mouse_position)
 
 
 func _zoom_out(mouse_position: Vector2) -> void:
-	_previous_target = _target_zoom
-	_target_zoom = maxf(_target_zoom - _zoom_increment, _minimum_zoom())
+	_target_zoom = maxf(_target_zoom - _zoom_increment, _minimum_zoom)
 
 	if _zoom_away_from_center:
 		# Assuming the camera's anchor mode is Drag Center,
 		# there is nothing we need to do
 		pass
 	else:
-		_zoom_to_cursor(mouse_position)
-
-	set_physics_process(true)
+		_add_movement(mouse_position)
 
 
 ## Makes the camera zoom to the cursor's position.
-func _zoom_to_cursor(mouse_position: Vector2) -> void:
-	var viewport_size: Vector2 = _camera.get_viewport_rect().size
+func _add_movement(mouse_position: Vector2) -> void:
+	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	var offset_pixels: Vector2 = mouse_position - viewport_size * 0.5
-	var current_zoom: Vector2 = Vector2.ONE / _previous_target
-	var new_zoom: Vector2 = Vector2.ONE / _target_zoom
-	_camera_movement += offset_pixels * (current_zoom - new_zoom)
-
-
-func _maximum_zoom() -> float:
-	# Prevents glitches for the edge case where the minimum zoom is
-	# larger than the maximum zoom. When this happens, we ignore
-	# the maximum zoom and stay at the minimum zoom.
-	return maxf(_maximum_zoom_limit, _minimum_zoom())
-
-
-## Returns the minimum zoom amount.
-## Currently, you can zoom out such that the world takes half the screen.
-func _minimum_zoom() -> float:
-	# Prevent division by zero
-	if (
-			_camera.world_limits.width() == 0.0
-			or _camera.world_limits.height() == 0.0
-	):
-		return 1.0
-
-	return 0.5 * minf(
-			_camera.get_viewport_rect().size.x / _camera.world_limits.width(),
-			_camera.get_viewport_rect().size.y / _camera.world_limits.height()
-	)
-
-
-func _keep_camera_in_bounds() -> void:
-	var minimum_zoom: float = _minimum_zoom()
-	if _camera.zoom.x < minimum_zoom or _camera.zoom.y < minimum_zoom:
-		_camera.zoom = minimum_zoom * Vector2.ONE
-		_target_zoom = minimum_zoom
-		return
-
-	var maximum_zoom: float = _maximum_zoom()
-	if _camera.zoom.x > maximum_zoom or _camera.zoom.y > maximum_zoom:
-		_camera.zoom = maximum_zoom * Vector2.ONE
-		_target_zoom = maximum_zoom
-
-
-func _connect_world_limits(world_limits: WorldLimits) -> void:
-	if world_limits == null:
-		push_error("World limits is null.")
-		return
-
-	if not world_limits.current_limits_changed.is_connected(_on_world_limits_changed):
-		world_limits.current_limits_changed.connect(_on_world_limits_changed)
-
-
-func _disconnect_world_limits(world_limits: WorldLimits) -> void:
-	if world_limits == null:
-		push_error("World limits is null.")
-		return
-
-	if world_limits.current_limits_changed.is_connected(_on_world_limits_changed):
-		world_limits.current_limits_changed.disconnect(_on_world_limits_changed)
-
-
-## Ensures the camera stays in bounds when the screen size changes.
-func _on_screen_size_changed() -> void:
-	_keep_camera_in_bounds()
-
-
-## Idem for when the world limits change.
-func _on_world_limits_changed(_world_limits: WorldLimits) -> void:
-	_keep_camera_in_bounds()
+	var zoom_before: Vector2 = Vector2.ONE / _previous_target
+	var zoom_after: Vector2 = Vector2.ONE / _target_zoom
+	_movement += offset_pixels * (zoom_before - zoom_after)
